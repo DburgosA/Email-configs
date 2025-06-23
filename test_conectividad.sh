6eee26b54f1fb19f567a51a751d701859e708d99
DburgosA <111473775+DburgosA@users.noreply.github.com>
2025-06-23 19:28:34 -0400

    ultimaaa
    ojala que si

--- test_conectividad.sh
+++ test_conectividad.sh
@@ -4,42 +4,42 @@
 set -e
 
 echo "== Instalando ping en todos los contenedores =="
-docker compose exec proxy-nginx apt-get update && docker compose exec proxy-nginx apt-get install -y iputils-ping
+docker compose exec proxy apt-get update && docker compose exec proxy apt-get install -y iputils-ping
 
-docker compose exec cms1-wordpress apt-get update && docker compose exec cms1-wordpress apt-get install -y iputils-ping
+docker compose exec cms1 apt-get update && docker compose exec cms1 apt-get install -y iputils-ping
 
-docker compose exec cms2-joomla apt-get update && docker compose exec cms2-joomla apt-get install -y iputils-ping
+docker compose exec cms2 apt-get update && docker compose exec cms2 apt-get install -y iputils-ping
 
-docker compose exec cms-db apt-get update && docker compose exec cms-db apt-get install -y iputils-ping
+docker compose exec db apt-get update && docker compose exec db apt-get install -y iputils-ping
 
 docker compose exec phpmyadmin apt-get update && docker compose exec phpmyadmin apt-get install -y iputils-ping
 
 echo "== Ping desde proxy a CMS1 (WordPress) =="
-docker compose exec proxy-nginx ping -c 2 cms1-wordpress
+docker compose exec proxy ping -c 2 cms1
 
 echo "== Ping desde proxy a CMS2 (Joomla) =="
-docker compose exec proxy-nginx ping -c 2 cms2-joomla
+docker compose exec proxy ping -c 2 cms2
 
-echo "== Ping desde proxy a la base de datos (cms-db, debe FALLAR) =="
-docker compose exec proxy-nginx ping -c 2 cms-db || echo "(Esperado: sin acceso)"
+echo "== Ping desde proxy a la base de datos (db, debe FALLAR) =="
+docker compose exec proxy ping -c 2 db || echo "(Esperado: sin acceso)"
 
 echo "== Ping desde CMS1 a CMS2 =="
-docker compose exec cms1-wordpress ping -c 2 cms2-joomla
+docker compose exec cms1 ping -c 2 cms2
 
 echo "== Ping desde CMS1 a la base de datos =="
-docker compose exec cms1-wordpress ping -c 2 cms-db
+docker compose exec cms1 ping -c 2 db
 
 echo "== Ping desde CMS2 a la base de datos =="
-docker compose exec cms2-joomla ping -c 2 cms-db
+docker compose exec cms2 ping -c 2 db
 
 echo "== Probar conexión MySQL desde CMS2 (Joomla) =="
-docker compose exec cms2-joomla apt update && \
-  docker compose exec cms2-joomla apt install -y default-mysql-client && \
-  docker compose exec cms2-joomla mysql -h cms-db -u joomla_user -pjoomla_pass -e 'SHOW DATABASES;'
+docker compose exec cms2 apt-get update && \
+  docker compose exec cms2 apt-get install -y default-mysql-client && \
+  docker compose exec cms2 mysql -h db -u joomla_user -pjoomla_pass -e 'SHOW DATABASES;'
 
 echo "== Probar conexión MySQL desde CMS1 (WordPress) =="
-docker compose exec cms1-wordpress apt update && \
-  docker compose exec cms1-wordpress apt install -y default-mysql-client && \
-  docker compose exec cms1-wordpress mysql -h cms-db -u wp_user -pwp_pass -e 'SHOW DATABASES;'
+docker compose exec cms1 apt-get update && \
+  docker compose exec cms1 apt-get install -y default-mysql-client && \
+  docker compose exec cms1 mysql -h db -u wp_user -pwp_pass -e 'SHOW DATABASES;'
 
 echo "== Pruebas finalizadas =="
