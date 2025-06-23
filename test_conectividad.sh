2281adbbf9e58450dac03d448d2047706c055404
DburgosA <111473775+DburgosA@users.noreply.github.com>
2025-06-23 19:36:05 -0400

    update
    xd

--- test_conectividad.sh
+++ test_conectividad.sh
@@ -3,17 +3,6 @@
 
 set -e
 
-echo "== Instalando ping en todos los contenedores =="
-docker compose exec proxy apt-get update && docker compose exec proxy apt-get install -y iputils-ping
-
-docker compose exec cms1 apt-get update && docker compose exec cms1 apt-get install -y iputils-ping
-
-docker compose exec cms2 apt-get update && docker compose exec cms2 apt-get install -y iputils-ping
-
-docker compose exec db apt-get update && docker compose exec db apt-get install -y iputils-ping
-
-docker compose exec phpmyadmin apt-get update && docker compose exec phpmyadmin apt-get install -y iputils-ping
-
 echo "== Ping desde proxy a CMS1 (WordPress) =="
 docker compose exec proxy ping -c 2 cms1
 
@@ -33,13 +22,9 @@ echo "== Ping desde CMS2 a la base de datos =="
 docker compose exec cms2 ping -c 2 db
 
 echo "== Probar conexión MySQL desde CMS2 (Joomla) =="
-docker compose exec cms2 apt-get update && \
-  docker compose exec cms2 apt-get install -y default-mysql-client && \
-  docker compose exec cms2 mysql -h db -u joomla_user -pjoomla_pass -e 'SHOW DATABASES;'
+docker compose exec cms2 mysql -h db -u joomla_user -pjoomla_pass -e 'SHOW DATABASES;'
 
 echo "== Probar conexión MySQL desde CMS1 (WordPress) =="
-docker compose exec cms1 apt-get update && \
-  docker compose exec cms1 apt-get install -y default-mysql-client && \
-  docker compose exec cms1 mysql -h db -u wp_user -pwp_pass -e 'SHOW DATABASES;'
+docker compose exec cms1 mysql -h db -u wp_user -pwp_pass -e 'SHOW DATABASES;'
 
 echo "== Pruebas finalizadas =="
